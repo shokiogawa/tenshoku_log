2f7864a92612ca724dc78a1a1ba74ccfa4af4685
shokiogawa <shouki2578@gmail.com>
2024-04-23 23:07:16 +0900

    Add: 環境変数

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyCnurNYi8QVe9jVn3suqICjBaK3DjR8Gq8")
+    GMSServices.provideAPIKey("")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
